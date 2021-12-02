c91ae82419c61ef31ee7c27cc9f0cbf12aae11a4
RafaelNatsu <rafaelnatsu99@gmail.com>
2021-12-01 22:43:48 -0300

    init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    //TODO receber arquivo
+    int qtdLojas, limiteMotos, qtdDrones, custoKmMoto, custoKmCaminhao;
+
+    return 0;
+}
